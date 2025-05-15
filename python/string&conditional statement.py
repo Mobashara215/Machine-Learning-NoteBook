824710b6087e63c6a60f08decf3ae9731828d022
Mobashara215 <mubasheramobashara121@gmail.com>
2025-05-16 02:22:30 +0600

    string&conditional statement

---
+++ python/string&conditional statement.py
@@ -0,0 +1,18 @@
+#1write a program to input 2 numbers & print their sum
+first=int(input("enter first:"))
+second=int(input("enter second:"))
+print("sum=",first+second)
+#2 input side of a square & print its area
+side=float(input("enter square side:"))
+print("area=",side*side)
+side=float(input("enter square side:"))
+print("area=",side**side)
+#practice 3 number 
+#write 2 input 2 floating point number & print their average 
+a=float(input("enter 1st :"))
+b=float(input("enter 2nd :"))
+print("avg=",(a+b)/2)#avg=4.5
+# 3 number print true if a>=b if not print false
+a=a=int(input("enter 1st :"))
+b=int(input("enter 2nd :"))
+print(a>=b)#3>4 tahole false
