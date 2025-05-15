887a5e25895d795093aeaa6c0780e69e6247d300
Mobashara215 <mubasheramobashara121@gmail.com>
2025-05-16 01:53:12 +0600

    variable & data

---
+++ python/data type & variable.py
@@ -0,0 +1,24 @@
+print("mobashara is my name .")
+name="mobashara"
+age=23
+price =34
+#kotation dile ota e print hoi
+#jde value print korte hoi nicher line er moto
+print(age)
+print("my name is:",name)
+print("my age is:",age)
+print(type(name))
+print(type(age))
+a=4
+b=2
+difference=a-b
+print(a+b)
+print(a-b)
+#Arithmetic operator
+a=5
+b=9
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
