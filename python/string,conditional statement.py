b89fad9c6848632059995cda0fdc07b61656ab32
Mobashara215 <mubasheramobashara121@gmail.com>
2025-05-16 12:30:26 +0600

    string,conditional statement

---
+++ python/string,conditional statement.py
@@ -0,0 +1,29 @@
+str1="This is a string .\n we are creating it in python"
+print(str1)
+
+ ## string concatination
+str1="Aymana"
+len1=len(str1)
+print(len1)
+str2="mobashara"
+len2=len(str2)
+print(len2)
+final_str=str1+" "+str2
+print(len(final_str))
+##indexing of a string 
+str1="Aymana"
+print(str1[3])
+##slicing 
+##amra jodi 1 theke 5 number porjonto mane ayman prjonto print korte cai tahole[1:6]
+##[1:4] mane 1 theke suru kore 4 dilam tarmane 4 er agher index porjonto print hbe
+str="aymana"
+print(str[1:4])##jde ami aymana puro print korte cai tahole ##print(str[0:6])
+## jde [:4]di tahole automatically suru theke 4 er aghe porjonto print hbeprint(str[:4])
+  ##jde last porjonto cai tahole (str[2:])
+print(str[2:])
+  ##negative index concept   apple
+                          ##-5 -4 -3 -2 -1 
+                          ##last er index include hoi na
+                          ## ami jde cai pl print korte tahole 
+str="apple"
+print(str[-3:-1])
