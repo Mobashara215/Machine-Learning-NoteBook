bc019577eb48c5f75d42ac3fb951a4c5b8c37c11
Aymana_Mobashara <136842404+Mobashara215@users.noreply.github.com>
2025-05-17 01:30:55 +0600

    dict

---
+++ python/dictionaries.py
@@ -0,0 +1,11 @@
+#Dictionaries are used to store data values in key:value pairs.
+#A dictionary is unordered, changeable and do not allow duplicates.
+info={
+    "name":"mobashara",
+    "subject":["science","cse","math"],
+    "topics":("dict","set"),
+    "age":22,
+    "is_adult":True
+}
+print(info)
+print(type(info))
