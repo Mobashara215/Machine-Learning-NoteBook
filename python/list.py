e77b551ab66fb57afc7b4c39535b4dd4423431be
Mobashara215 <mubasheramobashara121@gmail.com>
2025-05-16 20:16:47 +0600

    list

---
+++ python/list.py
@@ -0,0 +1,17 @@
+marks=[94,45,76,88,90]
+print(marks)
+print(len(marks))
+print(marks[0])
+print(marks[1])
+student=["mubashara",99,23,"USA"]
+print(student)
+#List items are ordered, changeable, and allow duplicate values.
+list=[2,1,3]
+print(list.append(5))
+print(list.sort(reverse=True))
+print(list)
+#descending order er jnno boro theke choto
+
+list=['f','b','c','d']
+print(list.sort())
+print(list)
