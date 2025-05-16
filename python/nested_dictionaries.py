793abb030cbe1128d0fe9b14fde8ab5f11499ec0
Aymana_Mobashara <136842404+Mobashara215@users.noreply.github.com>
2025-05-17 01:40:11 +0600

    dict

---
+++ python/nested_dictionaries.py
@@ -0,0 +1,12 @@
+#nested dictionaries
+student={
+    "name":"mobashara",
+    "subject":{
+        "physics":90,
+        "chem":91,
+        "math":89
+    }
+}
+print(student)
+#jdi ami math er marks print korte cai tahole print(student["subject"]["math"])
+print(student["subject"]["math"])
