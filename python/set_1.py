a7ff61ff077f2f93bf84a74e5783855c26c08788
Aymana_Mobashara <136842404+Mobashara215@users.noreply.github.com>
2025-05-17 20:39:22 +0600

    set_1

---
+++ python/set_1.py
@@ -0,0 +1,10 @@
+#Sets are used to store multiple items in a single variable.
+#A set is a collection which is unordered, unchangeable*, and unindexed.
+#Set items are unchangeable, but we can remove items and add new items.
+#Set items are unordered, unchangeable, and do not allow duplicate values.
+collection={1,2,2,2,"hello","world","world"}#ekhane e duplicate value thakle o print hbe 1 bar
+print(collection)
+print(len(collection))
+print(type(collection))
+#duplicate value ignore korbe sudhu ekbar print hbe
+#set hoche unordered
