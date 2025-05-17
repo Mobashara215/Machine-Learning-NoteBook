e6fd33cdd61fd4ac3d6c9ea8ed75b7037ce02cbd
Aymana_Mobashara <136842404+Mobashara215@users.noreply.github.com>
2025-05-17 20:53:17 +0600

    Add files via upload

---
+++ python/set_2.py
@@ -0,0 +1,12 @@
+collection=set()
+print(type(collection))
+#Set is the collection of the unordered items.
+#Each element in the set must be unique & immutable.
+collection=set()
+collection.add(1)
+collection.add(2)
+collection.add(2)
+print(collection)
+#jde ami ekhane 1 ke remove korte cai tahole 
+collection.remove(1)
+print(collection)
