969eaf740b6df35462de5225a4415d060b7e054e
Aymana_Mobashara <136842404+Mobashara215@users.noreply.github.com>
2025-05-17 20:12:55 +0600

    Add files via upload

---
+++ python/dictionaries_method.py
@@ -0,0 +1,12 @@
+student={
+"name":"mobashara",
+"subject":{
+    "math":98,
+    "chem":95
+}
+}
+print(student.items())#evabe sudhu pair ghulo print hbe 
+#student.item hole sudhu pair ghulo prnt hbe #tuple akare
+#jodi prothom pair ta k access dite cai 
+pairs=list(student.items())
+print(pairs[0])
