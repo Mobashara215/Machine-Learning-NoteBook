2efd219cdfb077134c4668d2cc3a88ad5b16887d
Aymana_Mobashara <136842404+Mobashara215@users.noreply.github.com>
2025-05-18 00:06:33 +0600

    continue

---
+++ python/continue.py
@@ -0,0 +1,11 @@
+
+#i=3 hbe na karon continue print 1 bar increment hoye 4 hbe
+
+i=0
+while i<=5:
+    if(i==3):
+        i+=1
+        continue
+    print(i)#tokhon ei conditon miss hoye jai
+    i+=1  #eta o miss hoye jai and uporerline er condition e chole jai i<=5 
+    # 0 theke 5 print hbe kintu 3 print hbe na
